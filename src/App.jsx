97509e16081054ec15837c715ebc38f0611c9c34
Aneesh <aneeshappid@gmail.com>
2024-12-08 11:04:49 +0530

    Added and styled the dice

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,24 @@
-
+import Die from '../Components/Die'
 import './App.css'
 
 function App() {
 
   return (
     <>
-      <main>Testing</main>
+      <main>
+        <div className="dice-container">
+          <Die value={1}/>
+          <Die value={2}/>
+          <Die value={5}/>
+          <Die value={2}/>
+          <Die value={6}/>
+          <Die value={2}/>
+          <Die value={4}/>
+          <Die value={4}/>
+          <Die value={3}/>
+          <Die value={6}/>
+        </div>
+      </main>
     </>
   )
 }
